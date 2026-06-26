08a4849a073c2b72d30dc9ea24bf7c35ce7b7410
mbaikyalo09-creator <mbaikyalo09@gmail.com>
2026-06-26 16:51:14 +0300

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <h1>Contact Us</h1>
+
+    <form>
+        <input type="text" placeholder="Your Name"><br><br>
+        <input type="email" placeholder="Your Email"><br><br>
+        <textarea placeholder="Your Message"></textarea><br><br>
+        <button type="submit">Send</button>
+    </form>
+
+    <br>
+    <a href="index.php">Back Home</a>
+
+</body>
+</html>
