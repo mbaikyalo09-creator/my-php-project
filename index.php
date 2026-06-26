58e0100d2f3b3f62079a73c907ba714598c2e488
mbaikyalo09-creator <mbaikyalo09@gmail.com>
2026-06-26 16:49:04 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First PHP Website</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <h1>Welcome to My PHP Website</h1>
+
+    <?php
+        echo "<p>Hello! This message was generated using PHP.</p>";
+    ?>
+
+    <nav>
+        <a href="index.php">Home</a> |
+        <a href="about.php">About</a> |
+        <a href="contact.php">Contact</a>
+    </nav>
+
+</body>
+</html>
