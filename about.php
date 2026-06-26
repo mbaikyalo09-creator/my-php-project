00fcbcf3820f664a9a00dda03b1acd69166b217e
mbaikyalo09-creator <mbaikyalo09@gmail.com>
2026-06-26 16:50:20 +0300

    Create about.php

---
+++ about.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Us</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <h1>About Us</h1>
+    <p>This is the about page.</p>
+
+    <a href="index.php">Back Home</a>
+
+</body>
+</html>
